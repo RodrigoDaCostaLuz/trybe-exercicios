c224872c1d207aeee52f0026809b9da7f7d5c827
Rodrigo Luz <rodrigoluz999@gmail.com>
2022-12-11 08:19:48 -0300

    requisitos 1,2,3 e 4 - do Exercício Desafio Técnico: manipulação de DOM

---
+++ modulo-1-fundamentos/secao-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/readme.md
@@ -0,0 +1,6 @@
+Exercícios - funções de manipulação do DOM
+Exercício 1
+1-Crie e execute uma função que mude o texto na tag <p>-----</p>, para uma descrição de como você se vê daqui a 2 anos;
+2-Crie e execute uma função que mude a cor do quadrado amarelo para rgb(76,164,109);
+3-Crie e execute uma função que mude a cor do quadrado vermelho para branco;
+4-Crie e execute uma função que corrija o texto da tag <h1>;
--- modulo-1-fundamentos/secao-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/script.js
+++ modulo-1-fundamentos/secao-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/script.js
@@ -1,6 +1,24 @@
-// Exercício 1
+// requisito 1
 const changeText = () => {
-    let paragraph = document.getElementsByTagName('p')[1];
-    paragraph.innerHTML = 'Ganhar 3.500 e estar em um empresa cabulosa!!';
-  };
-  changeText();
+  let paragraph = document.getElementsByTagName("p")[1];
+  paragraph.innerHTML = "escrever frase aqui";
+};
+changeText();
+// requisito 2
+const changeColorSquareYellow = () => {
+  let suqareYellow = document.getElementsByClassName("main-content ")[0];
+  suqareYellow.style.backgroundColor = "rgb(76,164,109)";
+};
+changeColorSquareYellow();
+// requisito 3
+const changeColorSquareRed = () => {
+  let suqareYellow = document.getElementsByClassName("center-content")[0];
+  suqareYellow.style.backgroundColor = "white";
+};
+changeColorSquareRed();
+// requisito 4
+const correctTextH1 = () => {
+  let textH1 = document.getElementsByTagName("h1")[0];
+  textH1.innerHTML = "Desafio - Javascript";
+};
+correctTextH1();
