bb56585f232651a1ab40ffb72f03761c507b96c2
Rodrigo Luz <rodrigoluz999@gmail.com>
2022-12-14 08:09:22 -0300

    exercícios seção 5 , dia 3 e 4

---
+++ modulo-1-fundamentos/secao-5-javascript-dom-eventos-e-web-storage/dia-4-javascript-web-storage/script4.js
@@ -0,0 +1,10 @@
+window.onload = () => {
+    btnBackgroundColor = document.getElementById('btn-background-color');
+    btnTextColor = document.getElementById('btn-text-color');
+    btnFontSize = document.getElementById('btn-font-size');
+
+
+
+
+
+}
